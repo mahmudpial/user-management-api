fcc25743827603b60ce369b9bfb59b501bb92fd3
pial <pialmahmud80@gmail.com>
2026-04-03 16:53:07 +0600

    Fix nginx PHP-FPM socket

--- docker/start.sh
+++ docker/start.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-# Run Laravel setup
 php artisan migrate --force
 php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 
-# Start PHP-FPM in background
 php-fpm -D
-
-# Wait for PHP-FPM to be ready
 sleep 2
 
-# Test nginx config
-nginx -t
-
-# Start Nginx in foreground
 nginx -g "daemon off;"
